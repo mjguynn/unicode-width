73bf3e9ef61972311e2cf33188f67b5b4512705c
Mateo Guynn <mjguynn@users.noreply.github.com>
2022-06-06 16:07:14 -0700

    Check-in LUT work before refactor

--- scripts/analyze.rs
+++ scripts/analyze.rs
@@ -45,14 +45,15 @@ impl Bits {
     fn count(&self) -> usize {
         self.indices.len()
     }
-    /// Creates a mask, where bit `i` is set iff bit `i` is included in this instance.
-    fn mask(&self) -> usize {
+    /// Copies selected bits from `target` into the leftmost contiguous bits of the output, with
+    /// all other bits set to zero.
+    fn extract(&self, target: usize) -> usize {
         let mut result = 0;
-        for &bit_index in self.indices.iter() {
-            result |= 1 << bit_index;
+        for (i, &bit_index) in self.indices.iter().enumerate() {
+            result |= ((target >> bit_index) & 1) << i;
         } 
         result
-    }
+    }  
 }
 impl<T: IntoIterator<Item = u8>> From<T> for Bits {
     fn from(input: T) -> Self {
@@ -65,18 +66,29 @@ impl<T: IntoIterator<Item = u8>> From<T> for Bits {
 struct BitComb<'a> {
     bits: &'a Bits,
     indices: Vec<usize>,
+    remaining: usize
 }
 impl<'a> BitComb<'a> {
     fn new(bits: &'a Bits, n: usize) -> Self {
         let mut indices = Vec::with_capacity(n);
-        let num_bits = bits.indices.len();
+        let num_bits = bits.count();
         // if bits has < n elements, or n==0, there are no combinations
         if num_bits >= n && n > 0 {
             (0..n).for_each(|i| indices.push(num_bits - (n - i)));
             //  corrects the first iteration so that we dont miss a combo
             indices[0] += 1;
         }
-        Self { bits, indices }
+        // calculates bits.count() choose n
+        let mut bits_f_div_n_f= 1;
+        for i in (n+1)..=bits.count() {
+            bits_f_div_n_f *= i;
+        }
+        let mut bits_minus_n_f = 1;
+        for i in 1..=(bits.count() - n) {
+            bits_minus_n_f *= i;
+        }
+        let remaining = bits_f_div_n_f / bits_minus_n_f;
+        Self { bits, indices, remaining }
     }
 }
 impl<'a> Iterator for BitComb<'a> {
@@ -89,51 +101,116 @@ impl<'a> Iterator for BitComb<'a> {
                     self.indices[j] = bit_index - (i - j + 1)
                 }
                 let selected = self.indices.iter().map(|&bi| self.bits.indices[bi]);
+                self.remaining -= 1;
                 return Some(Bits::from(selected))
             }
         }
         None
     }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.remaining, Some(self.remaining))
+    }
 }
+impl<'a> ExactSizeIterator for BitComb<'a> {}
 
-fn build_lut(widths: &[u8], bits: &Bits) -> (usize, Vec<usize>) {
-    // First, bucket the codepoints
-    let num_buckets = 1 << bits.count();
-    let bucket_size = (1 << UNICODE_BITS) / num_buckets;
-    let mut buckets = vec![Vec::with_capacity(bucket_size); char::MAX as usize];
-    let mask = bits.mask();
-    for codepoint in 0..(char::MAX as usize) {
-        buckets[codepoint & mask].push(widths[codepoint])
+#[derive(Clone)]
+struct LookupBucket {
+    chars: Vec<char>,
+    widths: Vec<u8>,
+}
+impl LookupBucket {
+    fn with_capacity(cap: usize) -> Self {
+        LookupBucket { chars: Vec::with_capacity(cap), widths: Vec::with_capacity(cap) }
     }
-    // Now, figure out which buckets have identical width patterns
-    let mut patterns = std::collections::HashMap::new();
-    let mut lut = Vec::with_capacity(num_buckets);
-    for pattern in buckets.into_iter() {
-        if pattern.is_empty() {
-            continue;
+    fn push(&mut self, ch: char, width: u8) {
+        if let Some(&l) = self.chars.last() {
+            assert!( l < ch )
         }
-        let len = patterns.len();
-        let index = *patterns.entry(pattern).or_insert(len);
-        lut.push(index);
+        self.chars.push(ch);
+        self.widths.push(width);
     }
-    (patterns.len(), lut)
-} 
-fn optimal_lut(widths: &[u8], usable: Bits, index_bits: usize) -> (Bits, Vec<usize>, usize) {
-    let mut opt = (Bits::from(0..1), Vec::new(), usize::MAX); // dummy value
-    for candidate in usable.combinations(index_bits) {
-        let (size, lut) = build_lut(widths, &candidate);
-        if size < opt.2 {
-            eprintln!("Lower! Size: {size}");
-            opt = (candidate, lut, size)
+    fn consume(&mut self, rhs: &mut LookupBucket) -> bool {
+        if self.widths.len() < rhs.widths.len() {
+            if self.widths.as_slice() == &rhs.widths[0..self.widths.len()] {
+                std::mem::swap(&mut self.widths, &mut rhs.widths);
+                self.chars.append(&mut rhs.chars);
+                self.chars.sort();
+                return true;
+            }
+        }
+        else {
+            if &self.widths[0..rhs.widths.len()] == rhs.widths.as_slice() {
+                self.chars.append(&mut rhs.chars);
+                self.chars.sort();
+                return true;
+            }
+        }
+        false
+    }
+}
+struct LookupTable {
+    /// Indices into buckets
+    table: Vec<usize>,
+    buckets: Vec<LookupBucket>
+}
+impl LookupTable {
+    fn new(widths: &[u8], chars: &[char], bits: &Bits) -> Self {
+        let num_buckets = 1 << bits.count();
+        // overestimation
+        let bucket_size = (1 << UNICODE_BITS) / num_buckets;
+        let mut raw_buckets = vec![LookupBucket::with_capacity(bucket_size); num_buckets];
+        for &ch in chars {
+            raw_buckets[bits.extract(ch as usize)].push(ch, widths[ch as usize])
+        }
+        let mut table = Vec::new();
+        let mut buckets: Vec<LookupBucket> = Vec::new();
+        'next_raw_bucket: for mut raw_bucket in raw_buckets.into_iter() {
+            for i in 0..buckets.len() {
+                if buckets[i].consume(&mut raw_bucket) {
+                    table.push(i);
+                    continue 'next_raw_bucket;
+                }
+            }
+            table.push(buckets.len());
+            buckets.push(raw_bucket);
+        }
+        Self { table, buckets }
+    }
+    fn bucket_count(&self) -> usize {
+        self.buckets.len()
+    }
+}
+fn optimal_lut(widths: &[u8], chars: &[char], usable: Bits, index_bits: usize) 
+    -> (Bits, LookupTable) 
+{
+    use std::io::Write;
+    eprintln!(""); // move cursor down one line
+
+    let mut combinations = usable.combinations(index_bits);
+    let num_combinations = combinations.len();
+
+    let mut min = (usize::MAX, None); // dummy value
+    for (i, combo) in combinations.enumerate() {
+        let table = LookupTable::new(widths, chars, &combo);
+        let key = table.bucket_count();
+        if key < min.0 {
+            min = (key, Some((combo, table)))
+        }
+        if i % 64 == 0 {
+            let pcnt = (i as f64) / (num_combinations as f64) * 100.0f64;
+            eprint!("\r\tProgress: {i}/{num_combinations} ({pcnt:.2}%)");
+            std::io::stderr().flush();
         }
     }
-    opt
+
+    eprint!("\x1BM"); // move cursor up one line
+    return min.1.unwrap();
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn optimal(widths: *const u8, widths_len: usize) {
     let widths = std::slice::from_raw_parts(widths, widths_len);
-    let (bits, lut, size) = optimal_lut(widths, Bits::from(0..UNICODE_BITS), 6);
-    eprintln!("Final: bits {bits:?}, size {size}");
-    eprintln!("{lut:?}");
+    let chars: Vec<_> = (0..u32::from(char::MAX)).filter_map(|c| char::try_from(c).ok()).collect();
+    let (bits, table) = optimal_lut(widths, &chars, Bits::from(0..UNICODE_BITS), 6);
+    eprintln!("OPTIMAL: {bits:?}, size {}", table.bucket_count());
 }
